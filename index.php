fed8884069fa23d42f3cf2b2e29fb9f4b8c2b506
GTech20 <ditchon16@gmail.com>
2019-02-05 02:42:29 +0800

    develop gab second commit with code

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <title>MIDTERM-PHP-ARRAY</title>
+    <body>
+        <div>
+        <?php
+            $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+            krsort($age);
+            
+            foreach($age as $x => $x_value){
+                echo "Key=" . $x . ", Value=" . $x_value;
+                echo "<br>";
+            }
+        ?>
+        </div>
+    </body>
+</html>
